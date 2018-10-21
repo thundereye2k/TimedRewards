324b97134eb96984963175a8bd28e14918956835
Bradley Steele <bradleysteele2000@gmail.com>
2018-10-21 04:10:34 +0100

    Added remaining time formatting

--- src/main/java/me/bradleysteele/timedrewards/menu/RewardItem.java
+++ src/main/java/me/bradleysteele/timedrewards/menu/RewardItem.java
@@ -17,11 +17,14 @@
 package me.bradleysteele.timedrewards.menu;
 
 import com.google.common.collect.Lists;
+import me.bradleysteele.timedrewards.backend.TRUserProfile;
+import me.bradleysteele.timedrewards.resource.yml.Locale;
 import org.bukkit.inventory.ItemStack;
 
 import javax.annotation.Nullable;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
 
 /**
@@ -54,6 +57,29 @@ public class RewardItem {
 
     public RewardItem(String key, long cooldown) {
         this(key, null, cooldown);
+    }
+
+    /**
+     * @param text    the text to format.
+     * @param profile the profile to base the formatting off of.
+     * @return formatted text depending on the remaining claim time.
+     */
+    public String format(String text, TRUserProfile profile) {
+        long ms = profile.getCooldownRemaining(this);
+
+        if (ms <= 0) {
+            return Locale.REWARD_COOLDOWN_NOW.getMessage().get(0);
+        }
+
+        int d = (int) TimeUnit.MILLISECONDS.toDays(ms);
+        int h = (int) (TimeUnit.MILLISECONDS.toHours(ms) - TimeUnit.DAYS.toHours(TimeUnit.MILLISECONDS.toDays(ms)));
+        int m = (int) (TimeUnit.MILLISECONDS.toMinutes(ms) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(ms)));
+        int s = (int) (TimeUnit.MILLISECONDS.toSeconds(ms) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(ms)));
+
+        return text.replaceAll(PATTERN_DAYS.pattern(), Integer.toString(d))
+                .replaceAll(PATTERN_HOURS.pattern(), String.format("%02d", h))
+                .replaceAll(PATTERN_MINUTES.pattern(), String.format("%02d", m))
+                .replaceAll(PATTERN_SECONDS.pattern(), String.format("%02d", s));
     }
 
     // Getters
