03856a9cdb9d3e886f0119799bea1d84df2ed140
Bradley Steele <bradleysteele2000@gmail.com>
2018-10-21 04:33:36 +0100

    Added reload to child commands

--- src/main/java/me/bradleysteele/timedrewards/command/timedrewards/CmdTimedRewards.java
+++ src/main/java/me/bradleysteele/timedrewards/command/timedrewards/CmdTimedRewards.java
@@ -1,8 +1,24 @@
+/*
+ * Copyright 2018 Bradley Steele
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package me.bradleysteele.timedrewards.command.timedrewards;
 
 import me.bradleysteele.commons.register.command.BCommand;
 import me.bradleysteele.commons.util.Players;
-import me.bradleysteele.timedrewards.resource.yml.Locale;
+import me.bradleysteele.timedrewards.command.CmdRewards;
 import org.bukkit.command.CommandSender;
 
 /**
@@ -16,10 +32,14 @@ public class CmdTimedRewards extends BCommand {
         this.setUsage("/timedrewards");
 
         this.setAllowConsole(true);
+
+        this.setChildren(new CmdTimedRewardsReload());
     }
 
     @Override
     public void execute(CommandSender sender, String[] args) {
-        Players.sendMessage(sender, Locale.CMD_UNKNOWN.getMessage());
+        if (Players.isOnline(sender)) {
+            CmdRewards.get().execute(sender, args);
+        }
     }
 }
